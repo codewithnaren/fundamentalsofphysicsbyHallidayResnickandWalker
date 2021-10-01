08731eea7181b5306197c5fbe774cc6d992d4dea
codewithnaren <91406174+codewithnaren@users.noreply.github.com>
2021-10-01 18:57:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = 'https://codewithnaren.github.io/socialandpoliticallife1/Polity6.pdf';
+const url = 'https://codewithnaren.github.io/fundamentalsofphysicsbyHallidayResnickandWalker/fundamentalsofphysicsbyHallidayResnickandWalker.pdf';
 
 
 let pdfDoc = null,
